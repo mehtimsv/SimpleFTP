c21cedbf3283c6681915f8450963705b638b202d
Mehti Msv <mehdimsv2001@gmail.com>
2021-04-01 05:34:30 +0430

    Add client socket connection & handle commands

--- client/client.py
+++ client/client.py
@@ -1,1 +1,46 @@
 from socket import *
+SERVER = '127.0.0.1'
+PORT = 2121
+
+def cmd_help():
+    print(
+        """
+List of the commands: 
+help            : Show the help
+list            : List files
+pwd             : Show current dir
+cd dir_name     : Change directory
+dwld file_path  : Download a file
+quit            : Exit
+    """)
+
+def cmd_exit():
+    print("Good Bye!")
+    exit()
+
+def send_request(cmd):
+    client.send(cmd.encode())
+    return client.recv(1024)
+
+def handle_response(res):
+    print(res)
+
+
+print("Trying to connect...")
+client = socket(AF_INET, SOCK_STREAM)
+try:
+    client.connect((SERVER, PORT))
+    print("Connection successful ;)")
+    cmd_help()
+except Exception as e:
+    print("Can't Connect! something's wrong with %s:%d. Exception is %s" % (SERVER, PORT, e))
+    exit()
+
+while True:
+    cmd = input("$cmd: ")
+    if cmd == 'help':
+        cmd_help()
+    elif cmd == 'quit':
+        cmd_exit()
+    else:
+        handle_response(send_request(cmd))
